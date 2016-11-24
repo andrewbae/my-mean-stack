3f7358cd694b880738caa77621f27c9a57054050
Andrew Bae <manner1130@gmail.com>
2016-11-24 11:20:09 +0900

    very hard

--- first-app/public/example/controller/example.client.controller.js
+++
@@ -1,7 +0,0 @@
-// ng-app="example"
-// ng-controller="ExampleController"
-angular.module('example').controller('ExampleController', ['$scope', 'Authentication',
-  function($scope, Authentication) {
-    $scope.name = Authentication.user ? Authentication.user.fullName : 'Mean Application';
-  }
-]);
